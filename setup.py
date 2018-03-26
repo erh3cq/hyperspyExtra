f301fa9ebefa1432284549ed4169aaaba49bd4f1
Eric R. Hoglund <erh3cq@virginia.edu>
2018-03-26 13:36:20 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+from setuptools import setup
+
+
+setup(name='hyperspyExtra',
+      version='1.0',
+      description='Extra functions for hyperspy (mostly personal use)',
+      author='Eric Hoglund',
+      author_email='erh3cq@virginia.edu',
+      packages=['hyperspyExtra'],
+	  install_requires=['hyperspy']
+     )
